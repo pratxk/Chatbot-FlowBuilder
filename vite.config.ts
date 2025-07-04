b29c9d448a35d060105df5f135fe8c6adc012f9b
pratxk <pratxk007@gmail.com>
2025-07-04 14:42:44 +0530

    Gh pagees config

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: 'Chatbot-FlowBuilder',
   server: {
     host: "::",
-    port: 8080,
+    port: 3000,
   },
   plugins: [
     react(),
